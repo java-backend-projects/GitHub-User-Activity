b894d0889f332c5bba136595a1c613360da69462
sug4chy <sb.9226981433@gmail.com>
2024-09-22 23:18:51 +0500

    init commit

---
+++ src/main/java/ru/sug4chy/Application.java
@@ -0,0 +1,12 @@
+package ru.sug4chy;
+
+public class Application {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
